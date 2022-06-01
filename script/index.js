7eba90ccb7f24ef5fc18e30309aa9e01a73a7849
Anna Lahmotko <annalahmotko@yandex.ru>
2022-06-01 22:16:01 +0300

    Правки. Ревью №1

--- script/index.js
+++ script/index.js
@@ -1,34 +1,32 @@
 const editButton = document.querySelector('.profile__button-edit-profile');
 const popup = document.querySelector('.popup');
 const editCloseButton = document.querySelector('.popup__close-button');
-
-editButton.addEventListener('click', function () {
-  popup.classList.add('popup_opened');
-})
-
-editCloseButton.addEventListener('click', function () {
-  popup.classList.remove('popup_opened');
-})
-
-
 let editForm = document.querySelector('.profile-form');
-let nameInput = document.querySelector('.profile-form__item_name');
-let descrInput = document.querySelector('.profile-form__item_descr');
+let nameInput = document.querySelector('.profile-form__input_type_name');
+let descrInput = document.querySelector('.profile-form__input_type_description');
 let profileName = document.querySelector('.profile__name');
-let profileDescr = document.querySelector('.profile__descr');
+let profileDescr = document.querySelector('.profile__description');
+
+nameInput.value = profileName.textContent;
+descrInput.value = profileDescr.textContent;
+
+function closePopup () {
+  popup.classList.remove('popup_opened');
+}
+
+function openPopup () {
+  popup.classList.add('popup_opened');
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
     profileName.textContent = nameInput.value;
     profileDescr.textContent = descrInput.value;
+
+    closePopup();
 }
 
-
 editForm.addEventListener('submit', formSubmitHandler);
-
-const saveButton = document.querySelector('.profile-form__save-button')
-
-saveButton.addEventListener('click', function () {
-  popup.classList.remove('popup_opened');
-})
+editButton.addEventListener('click', openPopup);
+editCloseButton.addEventListener('click', closePopup);
