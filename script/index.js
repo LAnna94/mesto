f35a40856bd72ce7a995a10b8550c91e2d891245
Anna Lahmotko <annalahmotko@yandex.ru>
2022-06-02 20:36:57 +0300

    Правки. Ревью №2

--- script/index.js
+++ script/index.js
@@ -1,20 +1,21 @@
 const editButton = document.querySelector('.profile__button-edit-profile');
 const popup = document.querySelector('.popup');
 const editCloseButton = document.querySelector('.popup__close-button');
-let editForm = document.querySelector('.profile-form');
-let nameInput = document.querySelector('.profile-form__input_type_name');
-let descrInput = document.querySelector('.profile-form__input_type_description');
-let profileName = document.querySelector('.profile__name');
-let profileDescr = document.querySelector('.profile__description');
+const editForm = document.querySelector('.profile-form');
+const nameInput = document.querySelector('.profile-form__input_type_name');
+const descrInput = document.querySelector('.profile-form__input_type_description');
+const profileName = document.querySelector('.profile__name');
+const profileDescr = document.querySelector('.profile__description');
 
-nameInput.value = profileName.textContent;
-descrInput.value = profileDescr.textContent;
 
 function closePopup () {
   popup.classList.remove('popup_opened');
 }
 
 function openPopup () {
+  nameInput.value = profileName.textContent;
+  descrInput.value = profileDescr.textContent;
+
   popup.classList.add('popup_opened');
 }
 
